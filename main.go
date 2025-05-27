08023ba22f8266c93d3781fc9bad61affe64ee37
Christophere Johnson <christophere.johnson@GS-DEV1.local>
2025-05-27 17:58:58 -0400

    mvp

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type Project struct {
-}
-
 func main() {
-	m := newModel()
-	p := tea.NewProgram(m)
-
-	if _, err := p.Run(); err != nil {
-		fmt.Println("Error:", err)
+	db, err := sql.Open("sqlite3", "./chronos.db")
+	if err != nil {
+		fmt.Println("error opening db:", err)
 		os.Exit(1)
 	}
+	initDB(db)
 
+	p := tea.NewProgram(initialModel(db))
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
+
+func initDB(db *sql.DB) {
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS projects (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT,
+			elapsed INTEGER
+		)
+	`)
+	if err != nil {
+		panic(err)
+	}
 }
